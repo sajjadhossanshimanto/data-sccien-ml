4671d8aa29d069a93cbd090fb70740b47b951da3
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-09-21 16:11:20 +0600

    lec 4

---
+++ numpy/3, lec 4 np operators.py
@@ -0,0 +1,23 @@
+#%%
+import numpy as np 
+
+arr = np.arange(11)
+arr
+# %% arithmetic
+arr+arr
+arr-arr
+arr*arr
+arr**3
+# %% Errors
+arr/arr
+1/arr
+# %% universal
+np.sort(arr)
+
+# %%
+#Calcualting exponential (e^)
+np.exp(arr)
+np.max(arr)
+np.sin(arr)
+np.log(arr)
+# %%
