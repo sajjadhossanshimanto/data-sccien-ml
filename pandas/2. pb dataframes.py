8318b7b7309fa3aa09529eecbe5e0c09dd0993b6
Sajjad hossan <s.m.sajjadhossanshimanto.c@gmail.com>
2024-10-03 22:25:48 +0600

    hier_index implimentation

--- pandas/2. pb dataframes.py
+++ pandas/2. pb dataframes.py
@@ -65,3 +65,22 @@ df.reset_index(inplace=True)
 df.set_index("W")
 # not in placed
 # %%
+
+
+
+# Index Levels
+outside = ['G1','G2']# Error -> musst matcha the shape 
+inside = [1,2,3,1,2,3]
+hier_index = list(zip(inside, outside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+hier_index
+#%%
+outside = ['G1','G1','G1','G2','G2','G2']
+inside = [1,2,3,1,2,3]
+hier_index = list(zip(outside,inside))
+hier_index = pd.MultiIndex.from_tuples(hier_index)
+hier_index
+#%%
+df = pd.DataFrame(np.random.randn(6,2),index=hier_index,columns=['A','B'])
+df
+# %%
